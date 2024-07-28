a22b38c806bd2eef214e578acb3c4abbb6d3aeb9
Matt-Tanti <66307285+Matt-Tanti@users.noreply.github.com>
2024-07-29 01:44:19 +0200

    Added filtering logic

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -1,56 +1,56 @@
 // https://en.wikipedia.org/wiki/Levenshtein_distance
 
-import { Game } from "../types/modelTypes";
+import { type Game } from '@/types/modelTypes'
 
 // Operations needed between two strings
-export const levenshteinDistance = (s1 = "", s2 = ""): number => {
-  const len1 = s1.length;
-  const len2 = s2.length;
+export const levenshteinDistance = (s1 = '', s2 = ''): number => {
+  const len1 = s1.length
+  const len2 = s2.length
 
-  let matrix = Array(len1 + 1);
+  let matrix = Array(len1 + 1)
   for (let i = 0; i <= len1; i++) {
-    matrix[i] = Array(len2 + 1);
+    matrix[i] = Array(len2 + 1)
   }
 
   for (let i = 0; i <= len1; i++) {
-    matrix[i][0] = i;
+    matrix[i][0] = i
   }
 
   for (let j = 0; j <= len2; j++) {
-    matrix[0][j] = j;
+    matrix[0][j] = j
   }
 
   for (let i = 1; i <= len1; i++) {
     for (let j = 1; j <= len2; j++) {
       if (s1[i - 1] === s2[j - 1]) {
-        matrix[i][j] = matrix[i - 1][j - 1];
+        matrix[i][j] = matrix[i - 1][j - 1]
       } else {
         matrix[i][j] = Math.min(
           matrix[i - 1][j] + 1,
           matrix[i][j - 1] + 1,
           matrix[i - 1][j - 1] + 1
-        );
+        )
       }
     }
   }
 
-  return matrix[len1][len2];
-};
+  return matrix[len1][len2]
+}
 
 export const fuzzySearch = (games: Game[], searchQuery: string) => {
   const distances = games.map((game: Game) => ({
     game,
-    distance: levenshteinDistance(searchQuery, game.title?.toLowerCase()),
-  }));
+    distance: levenshteinDistance(searchQuery, game.title?.toLowerCase())
+  }))
 
   // Dynamic threshold depending on search query length
   // More text = more error possibility
-  const threshold: number = Math.min(Math.floor(searchQuery.length / 3), 5);
+  const threshold: number = Math.min(Math.floor(searchQuery.length / 3), 5)
 
   const filteredGames = distances
     .filter(({ distance }) => distance <= threshold)
     .sort((a, b) => a.distance - b.distance)
-    .map(({ game }) => game);
+    .map(({ game }) => game)
 
-  return filteredGames;
-};
+  return filteredGames
+}
--- src/views/Lobby/LobbyView.vue
+++ src/views/Lobby/LobbyView.vue
@@ -1,8 +1,33 @@
 <script setup lang="ts">
-import { inject } from 'vue'
+import { type ContextState, type Game } from '@/types/modelTypes'
+import { fuzzySearch } from '@/utils/search'
+import { computed, inject, ref, type ComputedRef, type Ref } from 'vue'
 
-const state = inject('state')
-console.log(state)
+const state = inject<ContextState>('state')
+
+const searchFilter: Ref<string> = ref('')
+
+const filteredGames: ComputedRef<Game[]> = computed(() => {
+  if (!state?.defaultGames) return []
+
+  const defaultGamesArray: Game[] = Object.values(state.defaultGames)
+
+  // Return full array if no filter
+  if (!searchFilter.value) return defaultGamesArray
+
+  // Lowercase search query to remove case sensitivity
+  const searchQuery: string = searchFilter.value.toLowerCase()
+
+  // Initially match with substring
+  let fGames: Game[] = defaultGamesArray.filter((game: Game) =>
+    game.title?.toLowerCase().includes(searchQuery)
+  )
+
+  // Fuzzy search if no substring matches
+  if (fGames.length < 1) fGames = fuzzySearch(defaultGamesArray, searchQuery)
+
+  return fGames
+})
 </script>
 
 <template>
