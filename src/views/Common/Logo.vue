b12ff9ed40dba2845b268ffa92c887eac7993c65
Matt-Tanti <66307285+Matt-Tanti@users.noreply.github.com>
2024-07-29 22:34:46 +0200

    Added logo component

---
+++ src/views/Common/Logo.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import LogoImage from '@/assets/premier-gaming.png'
+</script>
+
+<template>
+  <img :src="LogoImage" alt="Premier Gaming" class="logo" />
+</template>
+
+<style scoped>
+.logo {
+  height: auto;
+  max-width: 300px;
+  min-width: 100px;
+}
+</style>
