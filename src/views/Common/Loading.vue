ca3e2db76c8bad7ed0e251d23263dbb3e6b53b42
Matt-Tanti <66307285+Matt-Tanti@users.noreply.github.com>
2024-07-29 23:24:14 +0200

    Fixed spinner not working

--- src/views/Common/Loading.vue
+++ src/views/Common/Loading.vue
@@ -22,14 +22,14 @@
   display: inline-block;
   box-sizing: border-box;
   animation: rotation 1s linear infinite;
+}
 
-  @keyframes rotation {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
