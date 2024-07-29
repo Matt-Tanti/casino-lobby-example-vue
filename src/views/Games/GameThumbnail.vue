e15e8ec752168f8a86c93cb4794b637d5625aa29
Matt-Tanti <66307285+Matt-Tanti@users.noreply.github.com>
2024-07-29 23:47:31 +0200

    Added game thumbnail

---
+++ src/views/Games/GameThumbnail.vue
@@ -0,0 +1,53 @@
+<!-- Game thumbnails to be used in a list -->
+<script setup lang="ts">
+import { type Game } from '@/types/modelTypes'
+import { useRouter } from 'vue-router'
+
+// Component props
+const props = defineProps<{ game: Game }>()
+
+const router = useRouter()
+
+// On thumbnail click navigate to game overview
+const handleClick = () => {
+  router.push(`/game/${props.game.slug}`)
+}
+</script>
+
+<template>
+  <div class="content" @click="handleClick">
+    <img class="image" :src="game.game_thumbnail" />
+    <div class="titleContainer">
+      <p class="title">
+        {{ game.title }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.content {
+  height: auto;
+  display: flex;
+  aspect-ratio: 1;
+  position: relative;
+}
+
+.image {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+
+.titleContainer {
+  position: absolute;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  width: 100%;
+}
+
+.title {
+  margin: 0.8rem;
+  text-align: start;
+}
+</style>
