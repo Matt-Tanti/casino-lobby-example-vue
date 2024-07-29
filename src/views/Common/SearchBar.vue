33ff7e812c8ee9c9c8b1279af21d635b7946c8e3
Matt-Tanti <66307285+Matt-Tanti@users.noreply.github.com>
2024-07-29 23:08:18 +0200

    Added search bar component

---
+++ src/views/Common/SearchBar.vue
@@ -0,0 +1,28 @@
+<!-- Simple input component -->
+<script setup lang="ts">
+const props = defineProps<{
+  searchFilter: string
+}>()
+</script>
+
+<template>
+  <div class="content">
+    <input type="text" placeholder="Search..." v-model="searchFilter" class="input" />
+  </div>
+</template>
+
+<style scoped>
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1.5rem;
+}
+
+.input {
+  flex: 1;
+  padding: 0.8rem;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+</style>
